db9d27921430bae720443ac91c54273d24904674
wangzheng2013 <wz2013013338@gmail.com>
2015-07-07 16:39:14 +0800

    no-yasuo-wanzhengban

---
+++ scripts/home.js
@@ -0,0 +1,10 @@
+function InBlk(bid)
+{
+var obj=document.getElementById(bid);
+obj.style.backgroundColor = "#0099FF";
+}
+function OutBlk(bid)
+{
+var obj=document.getElementById(bid);
+obj.style.backgroundColor = "#003399";
+}
